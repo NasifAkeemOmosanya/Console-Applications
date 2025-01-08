0ac2cfda8cfabeee1667decc05ac7e198fe7f991
Nasif Akeem-Omosanya <43321548+NasifAkeemOmosanya@users.noreply.github.com>
2025-01-08 23:57:47 +0100

    Updated logic-app.cs

--- logic-app.cs
+++ logic-app.cs
@@ -10,3 +10,30 @@ Console.WriteLine($"View English output:\n\t\t{englishLocation}\n");
 string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
 string russianLocation = $@"c:\Exercise\{projectName}\ru-RU\data.txt";
 Console.WriteLine($"{russianMessage}:\n\t\t{russianLocation}\n");
+
+// initialize variables - graded assignments 
+int currentAssignments = 5;
+
+int sophia1 = 93;
+int sophia2 = 87;
+int sophia3 = 98;
+int sophia4 = 95;
+int sophia5 = 100;
+
+int nicolas1 = 80;
+int nicolas2 = 83;
+int nicolas3 = 82;
+int nicolas4 = 88;
+int nicolas5 = 85;
+
+int zahirah1 = 84;
+int zahirah2 = 96;
+int zahirah3 = 73;
+int zahirah4 = 85;
+int zahirah5 = 79;
+
+int jeong1 = 90;
+int jeong2 = 92;
+int jeong3 = 98;
+int jeong4 = 100;
+int jeong5 = 97;
